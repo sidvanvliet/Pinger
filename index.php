01c865ccaec77ee81439d55528327939e83b19ca
Sid van Vliet <sid@vvliet.me>
2019-04-01 21:53:42 +0200

    Include name in title

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require('config/define.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>Pingui</title>
+    <title>Pingui - <?= NAME ?></title>
 
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <style>
